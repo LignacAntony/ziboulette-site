1c9d3e2eb8f22c29baf5d00ed56723b6066c13d2
marilyne.leam <marilyne.leam@eemi.com>
2024-10-24 19:23:41 +0200

    mobile first app

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,8 @@
+import { Arvo } from "next/font/google";
+
+export const arvo = Arvo({
+	weight: ['400', '700'],
+	style: [ 'normal', 'italic'],
+    subsets: ["latin"],
+    display: "swap",
+});
---
+++ src/app/qui-sommes-nous/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const QuiSommesNous: React.FC = () => {
+    return (
+        <div>
+            <h1>Qui sommes-nous ?</h1>
+            <p>
+                Nous sommes une entreprise innovante dédiée à [votre mission ou
+                objectif]. Fondée en [année], notre équipe s&apos;efforce de
+                [vos principales activités ou services].
+            </p>
+            <h2>Notre Vision</h2>
+            <p>[Décrivez votre vision d&apos;entreprise ici]</p>
+            <h2>Nos Valeurs</h2>
+            <ul>
+                <li>Valeur 1</li>
+                <li>Valeur 2</li>
+                <li>Valeur 3</li>
+            </ul>
+            <h2>Notre Équipe</h2>
+            <p>
+                [Présentez brièvement les membres clés de votre équipe ou la
+                structure de votre organisation]
+            </p>
+        </div>
+    );
+};
+
+export default QuiSommesNous;
