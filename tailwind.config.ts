53eef7eb9fa836b5c9e0cca7d44b59c4683a1149
marilyne.leam <marilyne.leam@eemi.com>
2024-11-05 00:25:40 +0100

    define accessible color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                primary: "#440FA7",
+                blue: "#002CDB",
+                red: "#9D1021",
+                green: "#105B16",
+				"zib-light-gray": "#fcfcfc",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
